2a6f2cc3a99c52b809c4eeaaa0eeea2c8e6302c5
guidomrobledo-blip <guidomrobledo@gmail.com>
2026-04-03 00:08:46 -0300

    Update logic_seguridad.py

--- logic_seguridad.py
+++ logic_seguridad.py
@@ -29,6 +29,8 @@ class PlanillaPDFSeguridad(FPDF):
     def header(self):
         if os.path.exists('carrefour+logo.png'):
             self.image('carrefour+logo.png', x=7, y=8, w=55)
+        if os.path.exists('checklist_seguridad.png'):
+            self.image('checklist_seguridad.png', x=75, y=8, w=18)    
 
         self.set_font("Times", 'B', 11)
         self.set_xy(100, 10)
