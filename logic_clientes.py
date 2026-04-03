c22f362a69ca8ac5122b7bc8ac45be1b1135be09
guidomrobledo-blip <guidomrobledo@gmail.com>
2026-04-03 01:42:45 -0300

    Update logic_clientes.py

--- logic_clientes.py
+++ logic_clientes.py
@@ -150,6 +150,9 @@ def generar_pdf_clientes(df, fecha_tit):
                 pdf.ln()
 
         for _, row in df_render.iterrows():
+             # 🔧 CONTROL DE SALTO PREVENTIVO (ANTES DE TODO)
+            if (pdf.h - pdf.get_y()) < (row_height + 3):
+            pdf.add_page()
             modalidad = row['MODALIDAD DE ENTREGA']
             banda = row['BANDA HORARIA']
 
